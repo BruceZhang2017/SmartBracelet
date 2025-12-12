a91f1f2d8c762cd08abe7f1d17aa6888eb1a34f9
bruce.zhang <bruce.zhang@anker-in.com>
2025-12-12 22:57:51 +0800

    添加多文案

--- SmartBracelet/Health/HealthViewController.swift
+++ SmartBracelet/Health/HealthViewController.swift
@@ -51,7 +51,7 @@ class HealthViewController: BaseViewController {
 
     private let femaleHealthTitleLabel: UILabel = {
         let label = UILabel()
-        label.text = "生理周期"
+        label.text = "female_cycle_title".localized()
         label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         label.textColor = .black
         return label
@@ -59,7 +59,7 @@ class HealthViewController: BaseViewController {
 
     private let femaleHealthSubtitleLabel: UILabel = {
         let label = UILabel()
-        label.text = "记录和预测月经周期"
+        label.text = "female_cycle_subtitle".localized()
         label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
         label.textColor = UIColor(red: 0.56, green: 0.59, blue: 0.63, alpha: 1.0)
         return label
